7f2c429e67b26d72f969c4c7f8d9be6ddb312419
MatveiSFDZ <60332701+Matveisf@users.noreply.github.com>
2026-03-17 10:35:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName)
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true); 
+console.log(typeof javascriptIsFun); 
+console.log(typeof 23);   
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
