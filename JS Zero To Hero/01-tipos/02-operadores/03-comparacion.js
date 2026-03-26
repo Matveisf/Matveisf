55f8fef11c593aec991cda1db5c393b1610328a9
MatveiSFDZ <60332701+Matveisf@users.noreply.github.com>
2026-03-26 11:00:28 -0500

    Add files via upload

---
+++ JS Zero To Hero/01-tipos/02-operadores/03-comparacion.js
@@ -0,0 +1,13 @@
+let a = 10;
+console.log(a == 10); // true
+console.log(a == '10'); // true
+console.log(a === 10); // true
+console.log(a === '10'); // false
+console.log(a != 10); // false
+console.log(a != '10'); // false
+console.log(a !== 10); // false
+console.log(a !== '10'); // true
+
+console.log(a != 10); // false
+console.log(a != '10'); // false
+console.log(a !== 10); // false
---
+++ JS Zero To Hero/01-tipos/02-operadores/04-logicos.js
@@ -0,0 +1,13 @@
+// AND, OR, NOT
+
+// AND (&&)
+
+let mayor = true;
+let suscrito = true;
+// console.log(true && true);
+// console.log(false && true);
+
+console.log(mayor && suscrito);
+
+// OR ||
+console.log(operador );
